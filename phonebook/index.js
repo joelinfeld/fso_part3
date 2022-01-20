41c424390d6d28f97a7e193a0cb95d4e4e7057e3
joelinfeld <joelinfeld@gmail.com>
2022-01-19 23:29:28 -0800

    GET and DELETE functionality

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+let persons = [
     {
     "name": "ding dosby",
     "number": "777",
@@ -33,9 +33,24 @@ app.get('/api/persons', (request, response) => {
     response.json(notes)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) response.json(person)
+    else response.status(404).end()
+})  
+
 app.get('/info', (request, response) => {
     const date = new Date()
     response.send(`Phonebook has info for ${notes.length} people<br/>${date}`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    response.status(204).end()
 })
 const PORT = 3001
 
