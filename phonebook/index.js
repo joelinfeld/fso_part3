fd6255b7a2ce34c984a0bea319258d7dc21b8d2d
joelinfeld <joelinfeld@gmail.com>
2022-01-19 23:07:25 -0800

    node phonebook app

---
+++ phonebook/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+    "name": "ding dosby",
+    "number": "777",
+    "id": 10
+    },
+    {
+    "name": "bam margera",
+    "number": "11111",
+    "id": 11
+    },
+    {
+    "name": "bing badly",
+    "number": "12415566136",
+    "id": 12
+    },
+    {
+    "name": "dddd",
+    "number": "1215",
+    "id": 13
+    }
+]
+
+let info = 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`Phonebook has info for ${notes.length} people<br/>${date}`)
+})
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
